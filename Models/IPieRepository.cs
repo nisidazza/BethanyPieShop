825c69ef13d3ebe3be8dbc0bf60ba39d2003bd14
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-02-28 11:57:43 +1300

    Implemented MockPieRepository class

--- Models/IPieRepository.cs
+++ Models/IPieRepository.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 
 namespace BethanysPieShop.Models
 {
-    public class IPieRepository
+    public interface IPieRepository
     {
+        IEnumerable<Pie> GetAllPies(); //this will return all pies 
+        Pie GetPieById(int pieId);  //this retrieves pies per Id
     }
 }
