6a71934615ff426e131395fd3f7748538d0887d9
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-02-28 15:02:56 +1300

    Creating the Controller - added private field IPieRepository and HomeController Constructor

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller //HomeController is a class that inherits from the base Controller class
+    {
+        private readonly IPieRepository _pieRepository;
+
+        // in the constructor we initialize the pieReposirtory in this way -  _pieRepository = new MockPieRepository(); - if we don't have a dependency injection
+        // the dependency injection will automatically inject an instance of the MockPieRepository here --> CONSTRUCTOR INJECTION
+        public HomeController(IPieRepository pieRepository) 
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
