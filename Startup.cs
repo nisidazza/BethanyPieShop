3f85d755d81d43314fe2c43bdfe0c58557de80fe
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-09 13:06:56 +1300

    Restored Startup.cs

---
+++ Startup.cs
@@ -0,0 +1,38 @@
+using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace BethanysPieShop
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IPieRepository, MockPieRepository>();//if someone is asking for an IPieRepository, a new MockPieRepository will be returned
+
+            //services.AddSingleton<>(); only one single instance is going to be created of the specified type, and only the same instance is going to be returned
+            //services.AddScoped<>(); //per request it will always return the sae instance, but as soon as the request goes out of scope, the instance is removed
+            services.AddMvc(); //in this way we enable MVC for this application
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
