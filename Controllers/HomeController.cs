3ce817675ddc7ca768be78d73220b692ee37d0ff
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-09 13:23:12 +1300

    Resolved Unexpected Exceptions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BethanysPieShop.Models;
+using Microsoft.Extensions.DependencyInjection;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
