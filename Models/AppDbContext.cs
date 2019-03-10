3c7a0676a2fd3a3ad00c8c1acdf6f6714a152781
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-10 16:22:40 +1300

    Added AppDbContext.cs to my Models folder and added my DbSet property so my EF Core can manage my pies in the database table Pies

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; // we need to add this in order to inherit from DbContext
+
+namespace BethanysPieShop.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base (options) // we pass DbContext Options through a Constructor
+        {
+            
+        }
+
+        public DbSet<Pie> MyProperty { get; set; }
+    }
+}
