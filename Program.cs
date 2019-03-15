8057189043718c1a9aabcf4bd24063dfe4e828f6
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-16 11:29:31 +1300

    Called the Seed method from Program.cs

--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace BethanysPieShop
@@ -14,7 +16,23 @@ namespace BethanysPieShop
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            // BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>(); 
+                    // I am using the dependency injection container to get access to the AppDbContext, which I need to pass to my DbInitializer
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //we could log this in a real-world situation
+                }
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
