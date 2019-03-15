91d7dd18717b8b1e42885a6b4e6f12cccab7fa34
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-16 12:05:09 +1300

    Added a new property and updated the database

--- Migrations/20190315225849_pieModelChanged.Designer.cs
+++ Migrations/20190315225849_pieModelChanged.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BethanysPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190315225849_pieModelChanged")]
+    partial class PieModelChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
