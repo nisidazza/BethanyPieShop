26b93b069e3658c12e0f83a7c7baa882e190d726
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-17 16:08:39 +1300

    Updated AppDbContext - it needs to inherit from IdentityDbContext

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore; // we need to add this in order to inherit from DbContext
 
 namespace BethanysPieShop.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext>options) : base (options) // we pass DbContext Options through a Constructor
         {
