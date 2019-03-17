d78c4987d65bde6786c57057b526bd210b31fd9a
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-17 15:10:02 +1300

    Created an IActionResult Index method for POST request
    Created an IActionResult FeedbackComplete method; when client submit a feedback a thank-you message is shown

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,11 +11,32 @@ namespace BethanysPieShop.Controllers
 {
     public class FeedbackController : Controller
     {
-        private readonly IfeedbackRepository // this controller will have a dependency on the FeedbackRepository
-        // GET: /<controller>/
+        private readonly IFeedbackRepository _feedbackRepository; // this controller will have a dependency on the FeedbackRepository
+        //we inject the feedbackRepository  dependency via constructor injection into this controller;
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+        // GET: /<controller>/ --> [HttpGet] attribute is the default, so no needs to specify it
         public IActionResult Index()
         {
             return View();
         }
+
+        // POST: /<controller>
+        // I need to specify that this method is going to ba called when a post is being received by using an HttpPost attribute
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            _feedbackRepository.AddFeedback(feedback); //pass this feedback into the feedbackRepository
+            return RedirectToAction("FeedbackCompete"); // I redirect the user to another action method - FeedbackComplete - that I have to create
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
+
+
     }
 }
