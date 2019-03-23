fbf1c31dd91b6cf395692471973b29da0a7aa5ec
Nisida Azzalini <nisida.azzalini@gmail.com>
2019-03-24 11:33:41 +1300

    Created separate Controllers for Login, Register and Logout

---
+++ Controllers/AccountLoginController.cs
@@ -0,0 +1,60 @@
+﻿using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class AccountLoginController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountLoginController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public object ModelState { get; private set; }
+
+        // GET: /<controller>/
+        //my AccountController will need to able to log in, so I replace the Index Method with Login;
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //I need to capture that data (in my Login.cshtml) in my AccountController.
+        //I need to create a second Action method when a Post request is sent to the Controller
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.LoginUserName);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.LoginPassword, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "User name/password not found");
+            return View(loginViewModel);
+        }
+    }
+}
+    
+
+
---
+++ Controllers/AccountLogoutController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class AccountLogoutController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        // Here I need an instance of these methods and I am going to use the dependency injection
+
+        public AccountLogoutController(SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
